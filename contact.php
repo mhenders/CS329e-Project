013535ca1b319bcb9aa35402d293af9a6eb0059b
mhenders <55204868+mhenders@users.noreply.github.com>
2021-05-02 22:58:03 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -11,14 +11,65 @@
   fwrite($file_w, $string2);
   fclose($file_w);
 
-  # Write thank you page
-  print <<<FORM_RESULT
+    print <<<FORM_RESULT
   <html>
   <head>
-  <title> Form Submitted </title>
+    <title>Form Submitted</title>
+    <meta charset='UTF-8'>
+    <meta name='Wayback Machine Contact Us' content='Contact Us'>
+    <meta name='author' content='Naranjo, Henderson, Siddiqi, Holzem'>
+    <script src="main.js"></script>
+    <link href='ACL.css' rel='stylesheet'>
+    <link href='contact.css' rel='stylesheet'>
   </head>
+
   <body>
-  <h1> Thank You for Your Input </h1>
+
+    <div id="countDown"></div>
+  <p id = "con">
+  <a href="contact.html">Contact Us</a>
+  </p>
+
+  <!-- Navigation Bar -->
+  <div class="NaviBar">
+
+    <!-- Left Links -->
+    <div class="dropdown">
+     <button class="dropbtn">Past Line Ups</button>
+     <div class="dropdown-content">
+       <button onclick=year(this.innerHTML)>2019</button>
+       <button onclick=year(this.innerHTML)>2018</button>
+       <button onclick=year(this.innerHTML)>2017</button>
+       <button onclick=year(this.innerHTML)>2016</button>
+     </div>
+    </div>
+    <form type='hidden' method='POST' action='artists.php' name='theForm'>
+      <input type='hidden' id='year' name="year" value=''>
+    </form>
+
+    <script>
+    function year(words){
+      console.log(words);
+      document.getElementById('year').value = words;
+      document.theForm.submit();
+    }
+    </script>
+
+      <!-- Centered logo -->
+      <div class="NaviBar-logo">
+        <a href="ACL.php"><img id='logo' src="logo.png"></a>
+      </div>
+
+      <!-- Right Links -->
+      <div class="NaviBar-right">
+        <a class = 'MyACL' href="myacl.php">MyACL</a>
+        <a class = 'newsletter' href="newsletter.php">Newsletter</a>
+      </div>
+  </div>
+  <br><br>
+  <h3> Thank You for Your Input
+  <br>
+  Click <a href = "ACL.html"> here </a> to return to the home page!</h3>
   </body>
   </html>
 FORM_RESULT;
