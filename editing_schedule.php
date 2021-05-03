d6b73655c717522d55917b95df144e238eb15fc6
Pedro Naranjo <pedronarac@gmail.com>
2021-05-03 00:17:22 -0500

    changed editing schedule

--- editing_schedule.php
+++ editing_schedule.php
@@ -1,10 +1,4 @@
 <?php
-   //error_reporting(E_ALL);
-   //ini_set("display_errors", "on");
-//echo "hey1";
- //  require 'myacl_login2.php';
-
-   echo "hello";
    $server = $_GET["server"];
    $user   = $_GET["user"];
    $pwd    = $_GET["pwd"];
@@ -12,22 +6,8 @@
 
    // Connect to MySQL Server
   $mysqli2 = new mysqli($server, $user, $pwd, $user);
-  echo "yes";
-
-   //if ($mysqli2->connect_errno) {
-  //    die('Connect Error: ' . $mysqli2->connect_errno . ": " .  $mysqli2->connect_error);
-  // } else {
-  // }
-//echo "here";
-
-echo $server;
-echo $pwd;
-echo $user;
 //Select Database
 $mysqli2->select_db($user) or die('Connect Error: ' . $mysqli2->error);
-
-
-echo "heyyy";
 
 // Retrieve data from Query String
 $username = $_GET['username'];
@@ -49,9 +29,6 @@ $password = $_GET['password'];
     $j = $_GET["j"];
     $k = $_GET["k"];
     $l = $_GET["l"];
-
-    echo "hello";
-    echo "nono". $a;
 
     //build query
     $query = "UPDATE acl SET a = '$a' WHERE username = '$username'";
@@ -127,5 +104,22 @@ $password = $_GET['password'];
 
 
     echo "Changes made";
+    echo "<form action = \"./schedule_week1_view.php\" method = \"post\">";
+    echo "<input type=\"hidden\" name = \"username\" value=$username>";
+    echo "<input type=\"hidden\" name = \"password\" value=$password>";
+    echo "<input type=\"hidden\" name = \"a\" value=$a>";
+    echo "<input type=\"hidden\" name = \"b\" value=$b>";
+    echo "<input type=\"hidden\" name = \"c\" value=$c>";
+    echo "<input type=\"hidden\" name = \"d\" value=$d>";
+    echo "<input type=\"hidden\" name = \"e\" value=$e>";
+    echo "<input type=\"hidden\" name = \"f\" value=$f>";
+    echo "<input type=\"hidden\" name = \"g\" value=$g>";
+    echo "<input type=\"hidden\" name = \"h\" value=$h>";
+    echo "<input type=\"hidden\" name = \"i\" value=$i>";
+    echo "<input type=\"hidden\" name = \"j\" value=$j>";
+    echo "<input type=\"hidden\" name = \"k\" value=$k>";
+    echo "<input type=\"hidden\" name = \"l\" value=$l>";
+    echo "<input type = \"submit\" value = \"See New Schedule\" />";
+    echo "</form>";
 ?>
    
